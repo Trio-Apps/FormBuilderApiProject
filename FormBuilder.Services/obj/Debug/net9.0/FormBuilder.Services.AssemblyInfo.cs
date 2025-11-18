6f4d9241aea52a160dab79857d7a6295e3e6012d
mahmoud <mm4084@fayoum.edu.eg>
2025-11-18 16:09:55 +0200

    update arc and change name folder2

--- FormBuilder.Services/obj/Debug/net9.0/FormBuilder.Services.AssemblyInfo.cs
+++ FormBuilder.Services/obj/Debug/net9.0/FormBuilder.Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FormBuilder.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5cb1c583d90141c12d0f21bb4074377a97f21ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1d3d30470e9b333517c2f607546e1607122b897")]
 [assembly: System.Reflection.AssemblyProductAttribute("FormBuilder.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FormBuilder.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
